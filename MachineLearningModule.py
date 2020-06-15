561ed94c340499aa91291a7c89e484c98b701222
Stephen <pentex_1@msn.com>
2020-06-15 13:56:07 +0100

    First initial test

--- MachineLearningModule.py
+++ MachineLearningModule.py
@@ -1,5 +1,7 @@
-hello raymond, can you see this?
+import matplotlib.pyplot as plt
 
-#i'm back
-not sure if i will have to save eventually
+x = [5,7,8,7,2,17,2,9,4,11,12,9,6]
+y = [99,86,87,88,111,86,103,87,94,78,77,85,86]
 
+plt.scatter(x, y)
+plt.show()

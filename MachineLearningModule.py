855f1e80f378ae338e9aa6212b3e838612233f15
Stephen <pentex_1@msn.com>
2020-06-15 15:12:36 +0100

    Parsing json data and printing out the magnitude values for all the earthquakes

--- MachineLearningModule.py
+++ MachineLearningModule.py
@@ -1,6 +1,13 @@
 import requests
-url = 'https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2020-06-10'
+import json
+
+url = 'https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2020-06-14'
 
 response = requests.get(url)
 
-print(response.content)
+#print(response.content)
+
+jsonData = json.loads(response.content)
+for feature in jsonData["features"]:
+    print(feature["properties"]["mag"])
+
